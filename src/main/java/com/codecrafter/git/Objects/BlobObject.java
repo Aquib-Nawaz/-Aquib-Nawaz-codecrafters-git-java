69801bf3eea4ba7276b0d363546e111cd39d9a9e
Aquib Nawaz <aquibnawaz78692@gmail.com>
2024-02-10 13:08:17 +0530

    Stage-3

--- src/main/java/com/codecrafter/git/Objects/BlobObject.java
+++ src/main/java/com/codecrafter/git/Objects/BlobObject.java
@@ -2,6 +2,7 @@ package com.codecrafter.git.Objects;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.zip.InflaterInputStream;
 
 public class BlobObject extends GitObjects{
@@ -14,14 +15,39 @@ public class BlobObject extends GitObjects{
     public void readObject() {
         try(InputStream stream = Files.newInputStream(file)) {
             InflaterInputStream inflatedStream = new InflaterInputStream(stream);
-            BufferedInputStream bufferedStream = new BufferedInputStream(inflatedStream);
-            int b;
-            while ((b = bufferedStream.read()) != -1) {
-                System.out.print((char) b);
+            Reader reader = new InputStreamReader(inflatedStream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            int readLen;
+            char [] type = new char[4];
+            readLen = bufferedReader.read(type, 0, 4);
+            assert (readLen == 4);
+            assert (Arrays.equals(type,"blob".toCharArray()));
+            int space = bufferedReader.read();
+            assert(space==(int)' ');
+            int reading = bufferedReader.read();
+
+            int fileSize=0;
+
+            while(reading !=-1){
+                reading -= (int)'0';
+                assert ( reading<10 && reading >=0);
+                fileSize = fileSize*10 +reading;
+                reading = bufferedReader.read();
+                if(reading==0)break;
             }
+
+            char [] fileContent = new char[fileSize];
+//            All the data read matches the size
+            assert fileSize == bufferedReader.read(fileContent, 0, fileSize);
+            System.out.print(fileContent);
+//            End of file reached
+            assert bufferedReader.read() == -1;
         }
         catch (IOException e){
-
+            System.out.println("Couldn't Read File\n");
+        }
+        catch(NumberFormatException e){
+            System.out.println("Wrong file format");
         }
     }
 
--- src/main/java/com/codecrafter/git/Objects/GitObjects.java
+++ src/main/java/com/codecrafter/git/Objects/GitObjects.java
@@ -23,6 +23,19 @@ public abstract class GitObjects {
         }
     }
 
+    public int charArrayToInt(char []data,int start,int end) throws NumberFormatException
+    {
+        int result = 0;
+        for (int i = start; i < end; i++)
+        {
+            int digit = (int)data[i] - (int)'0';
+            if ((digit < 0) || (digit > 9)) throw new NumberFormatException();
+            result *= 10;
+            result += digit;
+        }
+        return result;
+    }
+
     public abstract void readObject();
     public abstract void writeObject();
 
